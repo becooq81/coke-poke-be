5d6e4a3a4bfad66318079d1c22eefbd9c1e9aeed
becooq81 <becooq81@gmail.com>
2023-11-08 15:50:02 +0900

    Initial commit

---
+++ src/main/java/com/gdscys/cokepokebe/CokePokeBeApplication.java
@@ -0,0 +1,13 @@
+package com.gdscys.cokepokebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokePokeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokePokeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdscys/cokepokebe/CokePokeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscys.cokepokebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CokePokeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
